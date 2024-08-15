f9146ffcc62789dce945de048362867c105588bc
alfie1906 <alfiegr@hotmail.com>
2024-08-15 12:18:45 +0100

    Added docstring

--- data_ingestion/data_utils.py
+++ data_ingestion/data_utils.py
@@ -6,6 +6,20 @@ import polars as pl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def get_github_data(url: str) -> pl.DataFrame:
+    """
+    Function for pulling data from a GitHub CSV into a Polars DataFrame.
+
+    Parameters
+    ----------
+    url : str
+        The URL of the CSV file on GitHub
+
+    Returns
+    -------
+    pl.DataFrame
+        A Polars DataFrame containing the data from the CSV file.
+    """
+    url += "?raw=true"
     data = pd.read_csv(url, index_col=0)
 
     return pl.from_pandas(data)
